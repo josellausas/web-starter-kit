30986bfd8c3e18024694ef0aa7d7becbf528341f
zunware <jose@josellausas.com>
2020-05-21 21:53:27 -0500

    Added client debugging

--- src/client.ts
+++ src/client.ts
@@ -3,12 +3,11 @@ import { Question } from "../@types/Question";
 // Give it its own context
 (() : void =>{
   let questions : Question[] = [];
-
   async function init() : Promise<void> {
     const request = await fetch("/questions");
     questions = await request.json();
     console.log(questions);
+    debugger;
   }
-
   init();
 })();
