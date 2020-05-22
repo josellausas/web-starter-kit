524cd4c69e6411bb7a40a6dcce4a11ad88b5c9c5
zunware <jose@josellausas.com>
2020-05-22 14:07:46 -0500

    Added webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ const path = require("path");
 
 module.exports = {
   mode:"development",
-  entry: path.resolve(__dirname, "src"),
+  entry: path.resolve(__dirname, "src", "app"),
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/dist"
+    publicPath: "/"
   },
   resolve: {
-    extensions: ['.js', '.jsx', 'ts', 'tsx']
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
   },
   devServer: {
     historyApiFallback: true,
