df977396618cf90c126bf7963860dbd8980ea84b
zunware <jose@josellausas.com>
2020-05-21 19:59:58 -0500

    Added express server

--- buildScripts/server.ts
+++ buildScripts/server.ts
@@ -1,2 +1,10 @@
-const message : string = "Hello TS!";
-console.log(message);
+import * as express from "express";
+
+const port : string | number = process.env.port || 1337;
+
+const app = express();
+
+app.listen(port);
+app.use(express.static("public"))
+
+console.log(`Listening on port ${port}`);
